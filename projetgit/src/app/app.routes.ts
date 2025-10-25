1dcb88b437da6499fb00a71df88a57b6c83de086
ramyrezguiG <mohamedrami.rezgui@sesame.com.tn>
2025-10-25 12:01:55 +0100

    Creation d'une page login

--- projetgit/src/app/app.routes.ts
+++ projetgit/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Signup } from './signup/signup';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-    {path:'signup', component:Signup}
+    {path:'signup', component:Signup},
+    {path:'login', component:Login}
 ];
